bea7df44e0d0672ccb8792ed0e9cdd9b374d80e5
Hayttle <hayttle#gmail.com>
2022-10-10 11:47:13 -0300

    dashboard and profile pages

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,28 @@
+export const Card = ({ timeA, timeB, match }) => (
+  <div className="rounded-xl border border-gray-300 p-4 text-center space-y-4">
+    <span className="text-sm md:text-base text-gray-700 font-bold">
+      {match.time}
+    </span>
+    <div className="flex space-x-4 justify-center items-center">
+      <span className="uppercase">{timeA.slug}</span>
+      <img src={`/imgs/flags/${timeA.slug}.png`} alt="" />
+
+      <input
+        type="number"
+        className="bg-red-300/[0.2] w-[55px] h-[55px] text-red-700 text-xl text-center"
+        name=""
+        id=""
+      />
+      <span className="text-red-500 font-bold">X</span>
+      <input
+        type="number"
+        className="bg-red-300/[0.2] w-[55px] h-[55px] text-red-700 text-xl text-center"
+        name=""
+        id=""
+      />
+
+      <img src={`/imgs/flags/${timeB.slug}.png`} alt="" />
+      <span className="uppercase">{timeB.slug}</span>
+    </div>
+  </div>
+);
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,25 +1,26 @@
 import { Icon, Card } from "~/components";
 
-export const Dashboard = () => (
+export const Profile = () => (
   <>
     <header className="bg-red-500 text-white p-4">
       <div className="container max-x-3xl flex justify-between p-4">
         <img src="/imgs/logo-red.svg" className="w-28 md:w-40" alt="" />
-        <a href="/profile">
-          <Icon name="profile" className="w-10" />
-        </a>
       </div>
     </header>
 
     <main className="space-y-6">
       <section id="header" className="bg-red-500 text-white">
         <div className="container max-x-3xl space-y-2 p-4">
-          <span>Olá Hayttle</span>
-          <h3 className="text-2xl font-bold">Qual é o seu palpite</h3>
+          <a href="/dashboard">
+            <Icon name="back" className="w-10" />
+          </a>
+          <h3 className="text-2xl font-bold">Hayttle Soljnivisk</h3>
         </div>
       </section>
 
       <section id="content" className="container max-x-3xl p-4 space-y-4">
+        <h2 className="text-red-500 text-xl font-bold">Seus palpites</h2>
+
         <div className="p-4 flex space-x-4 items-center justify-center">
           <Icon name="arrowLeft" className="text-red-500 w-6" />
           <span className="font-bold">24 de novembro</span>
