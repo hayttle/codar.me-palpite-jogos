33e83b528310342854aada85e51ced2245d7c37c
Hayttle <hayttle#gmail.com>
2022-10-11 15:22:23 -0300

    update api

--- api/router.js
+++ api/router.js
@@ -1,8 +1,6 @@
-import Koa from "koa";
 import Router from "@koa/router";
 
-const app = new Koa();
-const router = new Router();
+export const router = new Router();
 
 router.get("/", async (ctx) => {
   ctx.body = "Hello World";
@@ -10,8 +8,3 @@ router.get("/", async (ctx) => {
 router.get("/users", async (ctx) => {
   ctx.body = "Users";
 });
-
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-app.listen(3000);
